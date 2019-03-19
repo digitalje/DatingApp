8cfd5fbfa6dc08436fd21d6444a6dfa10186541a
digitalje <digitalje@yahoo.com>
2019-03-19 16:10:07 -0400

    End of section 8

---
+++ DatingApp.API/helpers/AutoMapperProfiles.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using AutoMapper;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        // Automapper uses profiles to map one class into another
+        public AutoMapperProfiles() {
+            CreateMap<User, UserForListDto>()
+                // This is used when Automapper can't automatically resolve the property. This in case we wanted to get the URL
+                // for the photo that was mapped as the main one in the user's photo collection.
+                .ForMember(dest => dest.PhotoUrl, opt => {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                .ForMember(dest => dest.Age, opt => {
+                    opt.MapFrom(d => d.DateOfBirth.CalculateAge());
+                });
+            CreateMap<User, UserForDetailedDto>()
+                .ForMember(dest => dest.PhotoUrl, opt =>
+                {
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                .ForMember(dest => dest.Age, opt =>
+                 {
+                     opt.MapFrom(d => d.DateOfBirth.CalculateAge());
+                 });
+            CreateMap<Photo, PhotosForDetailDto>();
+        }
+    }
+}
